bf59b6deca939d3acd413e1f17b4bf9a983c98a3
NikoLaaks <niko23010@student.hamk.fi>
2024-02-09 09:38:41 +0200

    admin page done for now

--- pages/admin.php
+++ pages/admin.php
@@ -5,7 +5,6 @@ session_start();
 // Check if the user is logged in and is an admin
 if(isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == 1) {
     // User is logged in as an admin, show the admin page
-    echo "Tervetuloa admin sivulle!";
 } else {
     // User is not logged in as an admin, redirect them to another page
     header("Location:./kirjautumis.php"); // Redirect to the login page
@@ -27,13 +26,17 @@ include("../php/connect.php");
     <link rel="stylesheet" href="../css/styles.css">
     <style>
         main{
-            display: block;
-            text-align: center;
+            align-items: center;
+            flex-direction: column;
+            
+            
         }
-        .container{
+        #message_container{
             display: flex;
             flex-direction: column;
-            max-width: 1000px;
+            text-align: center;
+            margin: 50px;
+            
         }a:visited{
             color: #FF0000;
         }a:link{
@@ -54,16 +57,41 @@ include("../php/connect.php");
         tr:nth-child(even) {
             background-color: #dddddd;
         }
+        .table{
+            overflow-x: auto;
+        }form{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }.container{
+            text-align: center;
+            margin-bottom:25px;
+        }input{
+            width: 400px;
+        }#submit{
+            margin-bottom: 15px;
+            max-width: 150px;
+            height: 40px;
+        }label{
+            font-size: 25px;
+        }h2{
+            font-size: 30px;
+        }p{
+            color:red;
+            font-size: 20px;
+        }
     </style>
 </head>
 <body>
     <main>
+    
+    <div id="message_container">
     <h2>Viestit contact lomakkeelta</h2>
-    <div class="container">
+        <div class="table">
         <table>
             <tr>
                 <th>Poista?</th>
-                <th>user_id</th>
+                <th>email</th>
                 <th>Viesti</th>
             </tr>
         <?php
@@ -73,15 +101,16 @@ include("../php/connect.php");
             while ($rivi=mysqli_fetch_object($tulos)){ # loopataan rivien läpi
                 echo "<tr>";
                 echo "<td><a href='../php/poista_viesti.php?poistettava=$rivi->id'>Poista</a></td>\n
-                <td>$rivi->user_id</td> 
+                <td>$rivi->email</td> 
                 <td>$rivi->message</td>";
                 echo "</tr>";
             }
                 
         ?>
         </table>
+        </div>
     </div>
-
+    <div class="container">
     <h2>Ohjeet uutisen lisääminen</h2>
     <p>Kirjoita url muodossa osoite.fi eli vain www. jälkeen tulevat. <br>Muista antaa alt kohtaan lyhyt teksti mitä on kuvassa</p>
 
@@ -98,25 +127,30 @@ include("../php/connect.php");
         <label for="image">Kuva:</label>
         <input type="file" id="image" name="image" accept="image/*" required><br>
 
-        <input type="submit" value="Lisää Uutinen">
+        <input type="submit" value="Lisää Uutinen" id="submit">
         <!--Lisää tähän vielä joku preview tietokannassa olevista uutisista ja poisto mahdollisuus niille
         esim otsikon perusteella poisto mahdollisuus-->
-        <ol>
+        <h2>Sivuilla näkyvät uutiset</h2>
+        <table>
+            <tr>
+                <th>Poista?</th>
+                <th>title</th>
+            </tr>
             <?php
             
-
-
             $tulos=mysqli_query($yhteys, "SELECT * FROM uutiset ORDER BY id DESC");# haetaan uutiset taulusta kaikki rivit
             
-            while ($rivi=mysqli_fetch_object($tulos)){ # loopataan rivien läpi 
-                echo "<li>$rivi->title\n 
-                <a href='../php/poista_uutinen.php?poistettava=$rivi->id'>Poista</a>";
-            }
             
+            while ($rivi=mysqli_fetch_object($tulos)){ # loopataan rivien läpi
+                echo "<tr>";
+                echo "<td><a href='../php/poista_uutinen.php?poistettava=$rivi->id'>Poista</a></td>";
+                echo "<td>$rivi->title</td>";
+                echo "</tr>";
+            }
             ?>
-        </ol>
+        </table>
     </form>
-
+    </div>
     </main>
 </body>
 </html>
